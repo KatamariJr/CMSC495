ddfd8e14bbdd9ce3f0c8e37be09f28fffbb00199
Austin Green <austinjgreen94@yahoo.com>
2020-02-03 19:53:22 -0500

    Initial commit

---
+++ src/main/java/com/cmsc495/spacesim/Start.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cmsc495.spacesim;
+
+import javax.swing.*;
+
+public class Start {
+    
+    public static void main (String []args){
+        System.out.println("Hello World!");
+        JFrame f = new JFrame();
+        Panel p = new Panel();
+        f.add(p);
+        f.setSize(400, 400);
+        f.setVisible(true);
+    }
+    
+}
