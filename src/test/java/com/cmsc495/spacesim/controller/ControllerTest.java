f75f21398e3610a60fbacb5739a98b363017aaee
Austin Green <austinjgreen94@yahoo.com>
2020-02-17 21:21:51 -0500

    update test package with names

--- src/test/java/com/cmsc495/spacesim/controller/ControllerTest.java
+++ src/test/java/com/cmsc495/spacesim/controller/ControllerTest.java
@@ -33,22 +33,6 @@ public class ControllerTest {
     @org.junit.jupiter.api.AfterEach
     public void tearDown() throws Exception {
     }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
 
     /**
      * Test of SendShip method, of class Controller.
@@ -56,11 +40,6 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testSendShip() {
         System.out.println("SendShip");
-        Controller.Ship s = null;
-        Controller.Planet target = null;
-        Controller.Person[] people = null;
-        HashMap<Controller.Resource, Integer> resources = null;
-        Controller.SendShip(s, target, people, resources);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -71,9 +50,6 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testGetAllPeople() {
         System.out.println("GetAllPeople");
-        Controller.Person[] expResult = null;
-        Controller.Person[] result = Controller.GetAllPeople();
-        assertArrayEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -84,9 +60,6 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testGetAllShips() {
         System.out.println("GetAllShips");
-        Controller.Ship[] expResult = null;
-        Controller.Ship[] result = Controller.GetAllShips();
-        assertArrayEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -97,9 +70,7 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testGetAllPlanets() {
         System.out.println("GetAllPlanets");
-        Controller.Planet[] expResult = null;
-        Controller.Planet[] result = Controller.GetAllPlanets();
-        assertArrayEquals(expResult, result);
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -110,9 +81,7 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testGetAllResources() {
         System.out.println("GetAllResources");
-        HashMap<Controller.Resource, Integer> expResult = null;
-        HashMap<Controller.Resource, Integer> result = Controller.GetAllResources();
-        assertEquals(expResult, result);
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -123,8 +92,7 @@ public class ControllerTest {
     @org.junit.jupiter.api.Test
     public void testAddPlanet() {
         System.out.println("AddPlanet");
-        Controller.Planet p = null;
-        Controller.AddPlanet(p);
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
