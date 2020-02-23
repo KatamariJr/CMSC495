7cf3716ec7529dfb9e31364b3211f80a6e820ddc
RexannaMcGowan <RexannaMcGowan@192.168.1.13>
2020-02-23 18:38:30 -0500

    Updated default constructor and parameterized constructor as per the 19FEB meeting. Additionally, I adjusted the code going into the functions/methods at the end of the file (addResources, addPeople, removeResources, and removePeople) to coincide with Megan's branch with her work on these functions.
    Created a ShipTest.java

--- src/main/java/com/cmsc495/spacesim/model/Ship.java
+++ src/main/java/com/cmsc495/spacesim/model/Ship.java
@@ -1,7 +1,7 @@
 /*
  * CMSC495 SpaceSimProject
  * C.O.P.E. Team
- * 19FEB2020
+ * 23FEB2020
  * Ship.java holds the data that constructs the Ship Class
  */
 package com.cmsc495.spacesim.model;
@@ -20,13 +20,16 @@ public class Ship {
     public ArrayList<Person> people;
     
     // Parameterized Ship Constructor
-    public Ship (int speed, int fuelCapacity, int cargoCapacity, int peopleCapacity, String name, String shipSize) {
- 
+    public Ship (int fuelCapacity, int cargoCapacity, int peopleCapacity, String name, String shipSize) {
+        this.fuelCapacity = fuelCapacity;
+        this.cargoCapacity = cargoCapacity;
+        this.peopleCapacity = peopleCapacity;
+        this.name = name;
+        this.shipSize = shipSize;
     }
     
     // Default Ship Constructor with passed values
     public Ship () {
-        speed = 300000;
         fuelCapacity = 500;
         cargoCapacity = 15;
         peopleCapacity = 6;
@@ -52,15 +55,6 @@ public class Ship {
      **/
     public void setName (String name) {
         this.name = name;
-    }
-    
-    /**
-     * Setter for speed
-     * @param speed int
-     * @return void
-     **/
-    public void setSpeed (int speed) {
-        this.speed = speed;
     }
     
     /**
@@ -109,14 +103,6 @@ public class Ship {
     }
     
     /**
-     * Getter for speed
-     * @return this.speed
-     **/
-    public int getSpeed() {
-        return this.speed;
-    }
-    
-    /**
      * Getter for fuelCapacity
      * @return this.fuelCapacity
      **/
@@ -142,28 +128,95 @@ public class Ship {
     
     // add the given resources to the ship resources
     public void addResources(HashMap<String, Integer> r) {
-
+        /*
+         * original code to go through HashMap keySet
         for (String key: r.keySet()) {
             for (int i = 0; i <= cargoCapacity; i++) {
                 r.get(key);
             }
-        }
-    }
+        } */
+        
+        // code to coincide with Planet.java function addResources
+        int totalInt = 0;
+        HashMap<String, Integer> total = new HashMap();
+        
+        total = (HashMap)this.resources.clone();
+        
+        totalInt = total.size();
+        
+        for(String rec : r.keySet()){
+            if(total.containsKey(rec)){
+                
+            }else{
+                totalInt++;
+                total.put(rec, null);
+            }
+        }//End for loop to go through HashMap keys
+        
+        this.resources = total;
+    }//End addResources function
     
     // add the given people to the ship people
-    public void addPeople(ArrayList<Person> p) {
-        ArrayList<Person> shipPeopleAL = new ArrayList<Person>(peopleCapacity);
+    public void addPeople(ArrayList<Person> p) {        
+        // original function to add all people in the Person arrayList to the ship's people ArrayList
+        // this.people.addAll(p);
         
-        shipPeopleAL.addAll(p);
-    }
+        // code to coincide with Planet.java function addPeople
+        for(int i = 0; i < p.size(); i++){
+            Person p1 = p.get(i);
+            for(int j = 0; j < people.size(); j++){
+                Person p2 = people.get(j);
+            
+                if(p1.equals(p2)){
+                   //do nothing 
+                }else
+                    people.add(p1);
+            }//End inner for loop
+        }//End for outer Loop
+    }//End addPeople function
     
     // remove the given resources from the ship resources
     public void removeResources(HashMap<String, Integer> r) {
-        r.clear();
+        //code to coincide with Planet.Java function removeResources
+        int totalInt = 0;
+        int valOne = 0;
+        int valTwo = 0;
+        int valDiff = 0;
+        HashMap<String, Integer> diff = new HashMap();
+        
+        diff = (HashMap)this.resources.clone();
+        
+        totalInt = diff.size();
+        try{
+            for(String rec : r.keySet()){
+                if(diff.containsKey(rec)){
+                    valOne = r.get(rec);
+                    valTwo = diff.get(rec);
+                    valDiff = valOne - valTwo;
+                }
+            }//End for loop to go through HashMap differentiation
+        }catch(Exception E){
+            System.out.println("Error in removeResouces");
+        }   
+        
+        this.resources = diff;
     }
     
     // remove the given people from the ship people
     public void removePeople(ArrayList<Person> p) {
-        p.clear();
-    }
+        // original function to remove all people from ship's arrayList of people
+        // this.people.removeAll(p);
+        
+        // code to coincide with Planet.java function removePeople
+        for(int i = 0; i < p.size(); i++){
+            Person p1 = p.get(i);
+            for(int j = 0; j < people.size(); j++){
+                Person p2 = people.get(j);
+            
+                if(p1.equals(p2)){
+                  people.remove(p1);
+                }
+            }//End inner for loop
+        }//End for outer Loop
+    }//End removePeople function
 }
