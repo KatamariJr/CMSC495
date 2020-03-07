668259a7d0294c2b28e8be4dfddf96b1e177f7c9
Mad Preacher <Mad Preacher@WEAK-SAUCE>
2020-03-06 21:30:59 -0500

    There were some conflicts from the last merge so I pushed an earlier version of my code to compare with master again as the GUI is all messed up.

--- src/main/java/com/cmsc495/spacesim/view/UserInterface.java
+++ src/main/java/com/cmsc495/spacesim/view/UserInterface.java
@@ -65,11 +65,6 @@ public class UserInterface extends javax.swing.JFrame {
         supplyPanelInner = new javax.swing.JPanel();
         submitButton = new javax.swing.JButton();
         viewLogButton = new javax.swing.JButton();
-        mainWindowMenuBar = new javax.swing.JMenuBar();
-        fileMenu = new javax.swing.JMenu();
-        exitMenuItem = new javax.swing.JMenuItem();
-        optionsMenu = new javax.swing.JMenu();
-        loadDatabaseMenuItem = new javax.swing.JMenuItem();
 
         supplyPanelTestButton.setText("jButton1");
         supplyPanelTestButton.addActionListener(new java.awt.event.ActionListener() {
@@ -173,32 +168,6 @@ public class UserInterface extends javax.swing.JFrame {
             }
         });
 
-        fileMenu.setText("File");
-
-        exitMenuItem.setText("Exit");
-        exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                exitMenuItemActionPerformed(evt);
-            }
-        });
-        fileMenu.add(exitMenuItem);
-
-        mainWindowMenuBar.add(fileMenu);
-
-        optionsMenu.setText("Options");
-
-        loadDatabaseMenuItem.setText("Load Database");
-        loadDatabaseMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                loadDatabaseMenuItemActionPerformed(evt);
-            }
-        });
-        optionsMenu.add(loadDatabaseMenuItem);
-
-        mainWindowMenuBar.add(optionsMenu);
-
-        setJMenuBar(mainWindowMenuBar);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -218,16 +187,7 @@ public class UserInterface extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(0, 60, Short.MAX_VALUE)
                         .addComponent(submitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(shipPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(6, 6, 6)
-                                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(passengerPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(supplyPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(supplyPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -236,8 +196,6 @@ public class UserInterface extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(shipPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -437,14 +395,6 @@ public class UserInterface extends javax.swing.JFrame {
             return r;
     }//GEN-LAST:event_supplyPanelTestButtonActionPerformed
 
-    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
-        System.exit(0);
-    }//GEN-LAST:event_exitMenuItemActionPerformed
-
-    private void loadDatabaseMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loadDatabaseMenuItemActionPerformed
-        // TODO add your handling code here:
-        System.out.println("database load button clicked");
-    }//GEN-LAST:event_loadDatabaseMenuItemActionPerformed
 
     /**
      * @param args the command line arguments
@@ -505,14 +455,9 @@ public class UserInterface extends javax.swing.JFrame {
     private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.ButtonGroup buttonGroup3;
     private javax.swing.ButtonGroup buttonGroup4;
-    private javax.swing.JMenuItem exitMenuItem;
-    private javax.swing.JMenu fileMenu;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JMenuItem loadDatabaseMenuItem;
-    private javax.swing.JMenuBar mainWindowMenuBar;
-    private javax.swing.JMenu optionsMenu;
     private javax.swing.JPanel passengerPanel;
     private javax.swing.JPanel passengerPanelInner;
     private javax.swing.JScrollPane passengerPanelScroll;
